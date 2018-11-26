7557a5c16627182279d54c80f530297c358e6c95
Kem <Treehouse@localhost.localdomain>
2018-11-26 15:29:49 -0600

    Fixed resume stylesheet path. Added javascript path to all html files. Added test if DOM content has loaded in js.

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,9 +1,12 @@
-document.addEventListener('DOMContentLoaded', function(event) {
-  content.log('DOMContent loaded');
-  document.querySelector('html').className = 'js';
-  if (CSS.supports('display: flex') {
-    var html = document.querySelector('html');
-    var classes = html.className;
-    html.className = classes + ' flexbox';
-  })
+//document.addEventListener('DOMContentLoaded', function(event) {
+//  content.log('DOMContent loaded');
+//  document.querySelector('html').className = 'js';
+//  if (CSS.supports('display: flex') {
+//    var html = document.querySelector('html');
+//    var classes = html.className;
+//    html.className = classes + ' flexbox';
+//  })
+// });
+document.addEventListener('DOMContentLoaded', function () {
+  console.log("DOM Has loaded");
 });
