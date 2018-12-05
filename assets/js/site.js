92584ee0efee3f5a52c40a0da311565c476225de
Kem <Treehouse@localhost.localdomain>
2018-12-05 16:02:07 -0600

    Added buttons to dreamcars. Added base equations for the manual slideshow js. Modified css to fit the new images.

--- assets/js/site.js
+++ assets/js/site.js
@@ -11,11 +11,29 @@ document.addEventListener('DOMContentLoaded', function () {
   console.log("DOM Has loaded");
 });
 
+
+
 //Modify this to add a button to make text visible on click of a button
-menu_button.addEventListener('click', function(e){
-  nav_list.classList.toggle('visible');
-  e.preventDefault();
+//menu_button.addEventListener('click', function(e){
+//  nav_list.classList.toggle('visible');
+//  e.preventDefault();
+//});
+
+var buttonB = document.querySelector('.buttonback');
+var buttonN = document.querySelector('.buttonnext');
+
+var c1 = 0;
+
+
+buttonN.addEventListener('click', function(){
+  c1++;
+  if (c1 >= 3) {
+    c1 = 0;
+  };
+  console.log(c1);
 });
+
+
 
 
 //Add either an automatic or manual slideshow in dreamcars
